878469c4a19e854344c742191b7db4b5204c92aa
tanyamelnyk19 <79224689+tanyamelnyk19@users.noreply.github.com>
2021-10-10 14:16:25 +0300

    Updates

---
+++ static/js/main.4dbcd2c1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),a=c(3),i=c.n(a),o=(c(8),c(9),c(0));var s=function(){return Object(o.jsx)("div",{className:"App"})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.4dbcd2c1.chunk.js.map
